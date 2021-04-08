118c6b21bd98081897f3d0e02164c72afb289de5
Alexandre LAM <alexandrelam@outlook.com>
2021-04-08 13:10:13 +0200

    roll back to sqlite

--- core/settings.py
+++ core/settings.py
@@ -84,15 +84,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- login/views.py
+++ login/views.py
@@ -2,13 +2,14 @@ from rest_framework import viewsets
 from .serializers import HeroSerializer, RegisterSerializer
 from django.contrib.auth.models import User
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Hero
 
 
 # Create your views here.
 class HeroViewSet(viewsets.ModelViewSet):
     queryset = Hero.objects.all()
+    permission_classes = (IsAuthenticated,)
     serializer_class = HeroSerializer
 
 
